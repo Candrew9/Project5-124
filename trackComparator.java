9464857c9126b1efa90bfb82443354f8855dea3d
Candrew9 <38706312+Candrew9@users.noreply.github.com>
2018-12-01 13:23:30 -0500

    Update trackComparator.java
    added methods for the various types of sorting needed

--- trackComparator.java
+++ trackComparator.java
@@ -9,7 +9,7 @@ package prj5;
  * @version 2018.11.28
  *
  */
-public class trackComparator {
+public class TrackComparator {
 
     // fields
     DLList<Track> tracks;
@@ -22,7 +22,7 @@ public class trackComparator {
      * @param list
      *            a DLList containing tracks and their data
      */
-    public trackComparator(DLList<Track> list) {
+    public TrackComparator(DLList<Track> list) {
         tracks = list;
         array = new Track[tracks.size()];
         for (int i = 0; i < tracks.size(); i++) {
@@ -39,6 +39,7 @@ public class trackComparator {
      */
     public DLList<Track> sortByName() {
         Track temp;
+        
         
         for (int i = 1; i < array.length; i++) {
             temp = array[i];
@@ -58,7 +59,109 @@ public class trackComparator {
         
         DLList<Track> result = new DLList<Track>();
         for (int k = 0; k < array.length; k++) {
-            result.add(array[k]);
+            result.add(k, array[k]);
+        }
+        return result;
+        
+    }
+    
+    /**
+     * Sorts the list by Genre
+     * 
+     * @return DLList containing track data, sorted by genre and alphabetical
+     *         order
+     */
+    public DLList<Track> sortByGenre() {
+        Track temp;
+        
+        
+        for (int i = 1; i < array.length; i++) {
+            temp = array[i];
+            String tempName = temp.getGenre();
+            int j = 0;
+            for (j = i; j > 0; j--) {
+                if (tempName.compareTo(array[j - 1].getGenre()) < 0) {
+                    array[j] = array[j - 1];
+                }
+                else {
+                    break;
+                }
+               
+            }
+            array[j] = temp;
+        }
+        
+        DLList<Track> result = new DLList<Track>();
+        for (int k = 0; k < array.length; k++) {
+            result.add(k, array[k]);
+        }
+        return result;
+        
+    }
+    
+    /**
+     * Sorts the list by Artist
+     * 
+     * @return DLList containing track data, sorted by genre and alphabetical
+     *         order
+     */
+    public DLList<Track> sortByArtist() {
+        Track temp;
+        
+        
+        for (int i = 1; i < array.length; i++) {
+            temp = array[i];
+            String tempName = temp.getArtist();
+            int j = 0;
+            for (j = i; j > 0; j--) {
+                if (tempName.compareTo(array[j - 1].getArtist()) < 0) {
+                    array[j] = array[j - 1];
+                }
+                else {
+                    break;
+                }
+               
+            }
+            array[j] = temp;
+        }
+        
+        DLList<Track> result = new DLList<Track>();
+        for (int k = 0; k < array.length; k++) {
+            result.add(k, array[k]);
+        }
+        return result;
+        
+    }
+    
+    /**
+     * Sorts the list by Year
+     * 
+     * @return DLList containing track data, sorted by date and alphabetical
+     *         order
+     */
+    public DLList<Track> sortByYear() {
+        Track temp;
+        
+        
+        for (int i = 1; i < array.length; i++) {
+            temp = array[i];
+            String tempName = temp.getDate();
+            int j = 0;
+            for (j = i; j > 0; j--) {
+                if (tempName.compareTo(array[j - 1].getDate()) < 0) {
+                    array[j] = array[j - 1];
+                }
+                else {
+                    break;
+                }
+               
+            }
+            array[j] = temp;
+        }
+        
+        DLList<Track> result = new DLList<Track>();
+        for (int k = 0; k < array.length; k++) {
+            result.add(k, array[k]);
         }
         return result;
         
